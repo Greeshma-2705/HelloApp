83346067a4bb0b68fb509ea8f1e42bc43e4dfdb9
greeshma-2705 <greshmasalla@gmail.com>
2026-03-17 08:35:32 +0530

    Coded UC3:Display Hello with Command-Line Argument or default Message

---
+++ HelloAppUC3.java
@@ -0,0 +1,10 @@
+class HelloAPPUC3{
+    public static void main(String[] args) {
+        String name ="World";
+        if (args.length >0) {
+            name = args[0];
+            
+        }
+        System.out.println("hello, "+name+"!");
+    }
+}
