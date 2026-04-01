813161f36332b96c67c4b8b457fbf186184285fc
greeshma-2705 <greshmasalla@gmail.com>
2026-04-01 19:21:46 +0530

    initial commit

---
+++ HellloAppUc5.java
@@ -0,0 +1,23 @@
+public class HellloAppUc5 {
+    public static void main(String[] args) {
+
+            StringBuilder nameBuilder = new StringBuilder();
+
+            // Append all names with comma and space
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
+            }
+
+            String result;
+
+            // If names are provided, remove trailing ", "
+            if (nameBuilder.length() > 0) {
+                result = "Hello, " + nameBuilder.substring(0, nameBuilder.length() - 2) + "!";
+            } else {
+                result = "Hello, World!";
+            }
+
+            System.out.println(result);
+    }
+}
+
